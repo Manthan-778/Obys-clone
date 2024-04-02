e6c355b1aa43d48be1b5bafc1df28e2c2e6004b3
manthan-valand <manthanvaland785@gmail.com>
2024-04-02 14:02:27 +0530

    page 1 complete

--- script.js
+++ script.js
@@ -1,54 +1,79 @@
-let tl = gsap.timeline()
+function loadingAnimation() {
+    let tl = gsap.timeline()
 
-tl.from(".line h1", {
-    y: 150,
-    stagger: 0.2,
-    duration: 0.6,
-    delay: 0.5
-});
+    tl.from(".line h1", {
+        y: 150,
+        stagger: 0.2,
+        duration: 0.6,
+        delay: 0.5
+    });
 
 
 
-tl.from("#line1-part1 ", {
-    opacity: 0,
-    onStart: function () {
-        var cnt = document.querySelector("#counter")
-        counter = 0;
-        cnt.textContent = 0
-        setInterval(function () {
-            if (counter < 100) {
-                counter++;
-                if (counter <= 9) {
-                    cnt.textContent = "0" + counter;
+    tl.from("#line1-part1 ", {
+        opacity: 0,
+        onStart: function () {
+            var cnt = document.querySelector("#counter")
+            counter = 0;
+            cnt.textContent = 0
+            setInterval(function () {
+                if (counter < 100) {
+                    counter++;
+                    if (counter <= 9) {
+                        cnt.textContent = "0" + counter;
+                    }
+                    else {
+                        cnt.textContent = counter;
+                    }
                 }
-                else {
-                    cnt.textContent = counter;
-                }
-            }
-        }, 30)
+            }, 30)
 
-    }
-});
+        }
+    });
 
-tl.to(".line h2",{
-    animationName:"anime",
-    opacity:1
-})
+    tl.to(".line h2", {
+        animationName: "anime",
+        opacity: 1
+    })
 
 
-tl.to("#loader", {
-    opacity: 0,
-    duration: 0.4,
-    delay: 4
-});
+    tl.to("#loader", {
+        opacity: 0,
+        duration: 0.4,
+        delay: 4
+    });
 
-tl.from("#page1",{
-    delay:0.2,
-    opacity:1,
-    duration:0.5,
-    y:1600
-})
+    tl.from("#page1", {
+        delay: 0.2,
+        opacity: 1,
+        duration: 0.5,
+        y: 1600
+    })
 
-tl.to("#loader",{
-    display:"none"
-})
+    tl.to("#loader", {
+        display: "none"
+    })
+    
+    tl.from("#nav",{
+        opacity:0
+    })
+
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h1,#hero4 h1",{
+        y:120,
+        stagger:0.2
+    })
+
+
+}
+
+function cursorAnimation() {
+    document.addEventListener("mousemove", function (dets) {
+        gsap.to("#crsr", {
+            left: dets.x,
+            top: dets.y
+        })
+    })
+    Shery.makeMagnet("#nav-part2 h4");
+}
+
+loadingAnimation(); cursorAnimation();
